2f948a09250313f88cce692b7affb31c831f605b
Jaider Andrade Ferreira <jaideraf@gmail.com>
2025-01-21 16:44:37 -0300

    testing with oficial ocrmypad base image

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.post('/upload', upload.single('file'), async (req, res) => {
         args: [
           '--tesseract-timeout=0',
           '--skip-text',
-          '--skip-big',
+          '--skip-big=50',
           '--pdfa-image-compression=lossless',
           `--title="${title}"`,
           `--author="${author}"`,
